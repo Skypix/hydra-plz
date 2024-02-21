9e742dc98c520cb080886aaf21b06a4bd1382205
Skypix <skypix9player@gmail.com>
2024-02-21 10:15:45 +0100

    modified:   app/components/Footer.jsx 	new file:   app/routes/pages.blog.jsx

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -20,7 +20,7 @@ export function Footer({menu, shop}) {
             <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
                <li> 
                 
-                  <Link to={"/pages/catalog"} className="hover:underline me-4 md:me-6">Catalog</Link> 
+                  <Link to={"/pages/Blog"} className="hover:underline me-4 md:me-6">Blog</Link> 
                   
                 </li>
                 
---
+++ app/routes/pages.blog.jsx
@@ -0,0 +1,24 @@
+export default function Blog() {
+    return (
+        <div className="max-w-2xl px-6 py-16 mx-auto space-y-12">
+        <article className="space-y-8 dark:bg-gray-800 dark:text-gray-50">
+            <div className="space-y-6">
+                <h1 className="text-4xl font-bold md:tracki md:text-5xl">Boosting Performance and Muscle Growth</h1>
+                <div className="flex flex-col items-start justify-between w-full md:flex-row md:items-center dark:text-gray-400">
+                    <div className="flex items-center md:space-x-2">
+                        <img src="https://source.unsplash.com/75x75/?portrait" alt="" className="w-4 h-4 border rounded-full dark:bg-gray-500 dark:border-gray-700" />
+                        <p className="text-sm">Martin Siblík • 21.2.2024</p>
+                    </div>
+                    <p className="flex-shrink-0 mt-3 text-sm md:mt-0">2 min read </p>
+                </div>
+            </div>
+            <div className="dark:text-gray-100">
+                <p>Creatine is a widely used supplement known for its ability to enhance athletic performance and muscle growth. By increasing ATP production, creatine improves strength, power, and exercise performance, particularly during high-intensity activities. It may also aid in muscle recovery, reduce fatigue, and offer cognitive benefits. </p>
+                <div> <p>Typically taken in a loading phase of 20g/day for 5-7 days followed by a maintenance phase of 3-5g/day, creatine is generally safe but may cause mild side effects like gastrointestinal discomfort. When used alongside proper training and nutrition, creatine can be a valuable tool for athletes seeking to optimize their performance and muscle gains. Consulting a healthcare professional before starting supplementation is advisable.</p></div>
+                   
+            </div>
+        </article>
+        
+    </div>
+    );
+  }
