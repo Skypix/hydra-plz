cb031eceaea44f25f8b19e67e1fd64b80405df26
Skypix <skypix9player@gmail.com>
2024-03-22 10:19:49 +0100

    modified:   app/routes/pages.contact.jsx

--- app/routes/pages.contact.jsx
+++ app/routes/pages.contact.jsx
@@ -4,7 +4,7 @@ export default function Contact() {
         <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
           <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
           <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a technical issue? Want to send feedback? Need details about our HYDRA website? Let us know. </p>
-          <h3 className="mb-4 text-2xl tracking-tight font-bold text-center text-gray-900 dark:text-white">Email: martinsiblik1@gmail.com </h3>
+          <h3 className="mb-4 text-2xl tracking-tight font-bold text-center text-gray-900 ">Email: martinsiblik1@gmail.com </h3>
         </div>
       </section>
     );
